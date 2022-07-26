db9fe2e9d9538a4b6737b043f772a66cdd60e2be
Chaka37 <Ldkelly3@gmail.com>
2022-07-25 21:33:51 -0400

    ERC20 and ERC20 detailed, just the starters

---
+++ SOL/printCoinERC20.sol
@@ -0,0 +1,24 @@
+/*
+
+Set pragma
+
+pragma solidity ^0.5.0;
+
+Import 'ERC20' and 'ERC20Detailed' contracts from OpenZeppelin
+```solidity
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+```
+
+Define a contract named 'PRNTCoin' that inherits the OpenZeppelin `ERC20` and `ERC20Detailed` contracts.
+```solidity
+contract PrintCoin is ERC20, ERC20Detailed{
+
+    address payable owner;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You do not have permission to mint these tokens!");
+        _;
+    }
+}
+```
