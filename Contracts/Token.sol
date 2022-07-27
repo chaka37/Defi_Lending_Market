c6d957d0e0e62cd624cdf8164d47a1c8969f34d7
Chaka37 <Ldkelly3@gmail.com>
2022-07-27 19:27:44 -0400

    Fixed Code in token.sol

--- Contracts/Token.sol
+++ Contracts/Token.sol
@@ -1,19 +1,18 @@
-/*
+//
 
-Set pragma
+//Set pragma
 
-```solidity
 pragma solidity ^0.5.0;
-```
 
-Import 'ERC20' and 'ERC20Detailed' contracts from OpenZeppelin
-```solidity
+
+//Import 'ERC20' and 'ERC20Detailed' contracts from OpenZeppelin
+//solidity
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
-```
 
-Define a contract named 'PRNTCoin' that inherits the OpenZeppelin `ERC20` and `ERC20Detailed` contracts.
-```solidity
+
+//Define a contract named 'PRNTCoin' that inherits the OpenZeppelin `ERC20` and `ERC20Detailed` contracts.
+//solidity
 contract PRNTCoin is ERC20, ERC20Detailed{
 
     address payable owner;
@@ -32,5 +31,3 @@ contract PRNTCoin is ERC20, ERC20Detailed{
         _mint(recipient, amount);
     }
 }
-}
-```
