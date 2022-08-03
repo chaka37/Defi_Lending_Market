724bd65a4c5629e129a2f9e34392deed2eca1611
Chaka37 <Ldkelly3@gmail.com>
2022-08-03 19:24:48 -0400

    Debugged Borrowing.sol

--- Contracts/Borrowing.sol
+++ Contracts/Borrowing.sol
@@ -7,44 +7,129 @@ import "./loanContract.sol";
 import "./Collateral.sol";
 
 
-contract borrowing {
+contract Borrowing {
+    struct Rational {
+        uint256 numerator;
+        uint256 denominator;
+    }
 
-     address public onlyOwner;
-  
-     // Defining a constructor   
-     constructor() public{   
-        onlyOwner=msg.sender;
-     }
-        function borrow (uint256 _amount) external {
-                require(_amount <= _borrowLimit(), "Not enough collateral");
-                onlyOwner += _amount;
-                totalBorrowed += _amount;
-                
-        }
-        
+    address payable lender;
+    address borrower;
 
-        function repay(uint256 _amount) external {
-                require(onlyOwner > 0, "Doesnt have a debt to pay");
-                weth.transferFrom(onlyOwner, address(this), _amount);
-                (uint256 fee, uint256 paid) = calculateBorrowFee(_amount);
-                usersBorrowed[msg.sender] -= paid;
-                totalBorrowed -= paid;
-                totalReserve += fee;
-                _sendPRNT(_amount);
+    Rational public interestRate;
+
+    uint256 public dueDate;
+    uint256 paymentPeriod;
+
+    uint256 public remainingBalance;
+    uint256 minimumPayment;
+
+    PRNTCoin token;
+    uint256 collateralPerPayment;
+
+    constructor(
+        address payable _lender,
+        address _borrower,
+        uint256 interestRateNumerator,
+        uint256 interestRateDenominator,
+        uint256 _paymentPeriod,
+        uint256 _minimumPayment,
+        uint256 principal,
+        PRNTCoin _token,
+        uint256 units
+    )
+        public
+    {
+        lender = _lender;
+        borrower = _borrower;
+        interestRate = Rational(interestRateNumerator, interestRateDenominator);
+        paymentPeriod = _paymentPeriod;
+        minimumPayment = _minimumPayment;
+        remainingBalance = principal;
+        token = _token;
+        collateralPerPayment = units;
+
+        uint256 x = minimumPayment * collateralPerPayment;
+        require(x / collateralPerPayment == minimumPayment,
+            "minimumPayment * collateralPerPayment overflows");
+
+        dueDate = block.timestamp + paymentPeriod;
+    }
+
+    function multiply(uint256 x, Rational memory r) internal pure returns (uint256) {
+        return x * r.numerator / r.denominator;
+    }
+    //function multiply(Rational memory numerator, Rational memory denominator, uint256 x) internal pure returns (uint256) {
+    //    return x * r.numerator / r.denominator;
+    //}
+
+    function calculateComponents(uint256 amount)
+        internal
+        view
+        returns (uint256 interest, uint256 principal)
+    {
+        interest = multiply(remainingBalance, interestRate);
+        require(amount >= interest);
+        principal = amount - interest;
+        return (interest, principal);
+    }
+
+    function calculateCollateral(uint256 payment)
+        internal
+        view
+        returns (uint256 units)
+    {
+        uint256 product = collateralPerPayment * payment;
+        require(product / collateralPerPayment == payment, "payment causes overflow");
+        units = product / minimumPayment;
+        return units;
+    }
+
+    function processPeriod(uint256 interest, uint256 principal, address recipient) internal {
+        uint256 units = calculateCollateral(interest+principal);
+
+        remainingBalance -= principal;
+
+        dueDate += paymentPeriod;
+
+        require(token.transfer(recipient, units));
+    }
+
+    function makePayment() public payable {
+        require(block.timestamp <= dueDate);
+
+        uint256 interest;
+        uint256 principal;
+        (interest, principal) = calculateComponents(msg.value);
+
+        require(principal <= remainingBalance);
+        require(msg.value >= minimumPayment || principal == remainingBalance);
+
+        processPeriod(interest, principal, borrower);
+    }
+
+    function withdraw() public {
+        lender.transfer(address(this).balance);
+    }
+
+    function missedPayment() public {
+        require(block.timestamp > dueDate);
+
+        uint256 interest;
+        uint256 principal;
+        (interest, principal) = calculateComponents(minimumPayment);
+
+        if (principal > remainingBalance) {
+            principal = remainingBalance;
         }
-        
-        function liquidation(address _user) external payable onlyOwner{
-                uint256 wethPrice = uint256(_getLatestPrice());
-                uint256 collateral = usersCollateral[_user];
-                uint256 borrowed = usersBorrowed[_user];
-                uint256 collateralToUsd = mulExp(wethPrice, collateral);
-                if (borrowed > percentage(collateralToUsd, maxLTV)) {
-                _withdrawWeth(collateral);
-                uint256 amountPRNT = _convertEthToPRNT(collateral);
-                totalReserve += amountPRNT;
-                usersBorrowed[_user] = 0;
-                usersCollateral[_user] = 0;
-                totalCollateral -= collateral;
-                }
-        }
+
+        processPeriod(interest, principal, lender);
+    }
+
+    function returnCollateral() public {
+        require(remainingBalance == 0);
+
+        uint256 amount = token.balanceOf(address(this));
+        require(token.transfer(borrower, amount));
+    }
 }
