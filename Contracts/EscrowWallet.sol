70dece43ab1d04ceb4e2799b2256f39855fe357f
the1tempest <tempestmb15@gmail.com>
2022-08-05 16:07:56 -0400

    Updated Escrow Wallet

--- Contracts/EscrowWallet.sol
+++ Contracts/EscrowWallet.sol
@@ -2,53 +2,49 @@
 
 pragma solidity ^0.8.0;
 
-import "./Collateral.sol";
-import "./Exchange_Rate.sol";
 import "./Token.sol";
-import "./loanContract.sol";
+import "./Borrowing.sol";
+import "./Exchange_Rate.sol";
 
+contract Escrow {
 
-contract Escrow is ERC20, ExchangeRateOracle{
-    address public escrowWallet;
-    bool public initialized = false;
-
-// Escrowed is the trigger of escrow
-// Loan Paid is the funing of Lender to escrow
-// Inialization is the creation of escrow wallet
-
-    event Escrowed(address _lender, address _borrower, uint256 _loanAmount, uint256 getLatestPrice);
-    event LoanPaid( address _borrower, uint256 loanAmount);
-    event initial(address _escrowWallet);
-
-    modifier isInitialized() {
-        require(initialized, "Contract is not yet initialized");
-        _;
+    enum Statuses {
+        awaitingPayment, 
+        paymentDelivery,
+        missedPayment,
+        releasedCollat,
+        loanPaid
     }
 
-    function initialize(address escrowWalletAddress) external escrowWallet{
-        require(!initialized, "Contract is already initialized");
+    Statuses public currentStatus;
 
-        address = escrowWalletAddress;
-
-        isinitialize = true;
-
-        emit (escrowWalletAddress);
+    modifier madePayment (){
+        require(currentStatus == Statuses.paymentDelivery, "Payment not received")
     }
 
-    function escrowDepositCollateral(address _borrower, uint256 _loanDuration) external payable isInitialized{
-        require(_borrower != address(0), "Cannot escrow to a zero address.");
-        require(msg.value > 0, "Cannot excrow 0 ETH");
-
-        uint256 loanAmount = msg.value;
-        uint256 getLatestPrice = block.timeStamp + _loanDuration;
-
-        //Wallet.mint(_borrower, loanAmount, getLatestPrice);
-
-        emit Escrowed(msg.sender,
-            _borrower,
-            loanAmount,
-            _loanDuration);
+    modifier delinquent (paymentMissed, gracePeriod){
+        require(currentStatus == Statuses.missedPayment," Payment received " )
     }
 
-    //function redeemEthEscrow();
+    address payable public owner;
+
+     constructor() {
+        owner = payable(msg.sender);
+        currentStatus = Statuses.awaitingPayment;
+    }
+
+    function ReleaseCollat public payable madePayment {
+        //releases when payment is made
+        currentStatus = Statuses.releasedCollat;
+        // an action to release the collateral
+        (transfer.borrower(collateralPerPayment)
+
+        require(transfer)
+
+    }
+
+    function LoanDefault {
+
+        // when in delinquency 
+    }
 }
